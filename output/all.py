c41bb118507401f1cafeec6834e95a62d9beec88
Greg Siegfried <40606199+siegfriedgreg@users.noreply.github.com>
2018-07-30 01:35:32 -0400

    Update all.py
    modified to use my mapset

--- output/all.py
+++ output/all.py
@@ -2,17 +2,22 @@
 
 import os
 
-datasets = ['soc-LiveJournal1',
-            'kron_g500-logn21',
-            'delaunay_n21',
-            'delaunay_n24',
-            'belgium_osm',
-            'europe_osm',
-            'road_usa',
-            'cit-Patents',
-            'ak2010',
-            'delaunay_n13',
-            'coAuthorsDBLP']
+datasets = ['gh001',
+            'gh002',
+            'gh003',
+            'gh004',
+            'gh005',
+            'gh006',
+            'gh007',
+            'gh008',
+            'gh009',
+            'gh010',
+            'gh011',
+            'gh012',
+            'gh013',
+            'gh014',
+            'gh015'
+            'gh016']
 
 options = {
     "direction_optimizing_bfs" : "--src=0 --idempotence",
@@ -28,11 +33,11 @@ for binary in ["direction_optimizing_bfs", "breadth_first_search"]:
     for mark_pred in ["", "--mark-pred"]:
         for directed in ["", "--undirected"]:
             for dataset in datasets:
-                os.system("../../gunrock-build/bin/%s market ../dataset/large/%s/%s.mtx %s %s %s --iteration-num=10 --quiet --jsondir=." % (binary, dataset, dataset, options[binary], mark_pred, directed))
+                os.system("../../gunrock/build/bin/%s market ../../maps/%s.mtx %s %s %s --iteration-num=10 --quiet --jsondir=." % (binary, dataset, options[binary], mark_pred, directed))
 
 for binary in ["betweenness_centrality",
                "connected_component",
                "pagerank",
                "single_source_shortest_path"]:
     for dataset in datasets:
-        os.system("../../gunrock-build/bin/%s market ../dataset/large/%s/%s.mtx %s --iteration-num=10 --quiet --jsondir=." % (binary, dataset, dataset, options[binary]))
+        os.system("../../gunrock/build/bin/%s market ../../maps/%s.mtx %s --iteration-num=10 --quiet --jsondir=." % (binary, dataset, options[binary]))
