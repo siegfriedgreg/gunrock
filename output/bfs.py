5ba48256efa79ce382593f9194532eeb4aa7651f
Greg Siegfried <40606199+siegfriedgreg@users.noreply.github.com>
2018-07-30 01:39:07 -0400

    Update bfs.py

--- output/bfs.py
+++ output/bfs.py
@@ -5,15 +5,20 @@ import os
 for binary in ["direction_optimizing_bfs", "breadth_first_search"]:
     for mark_pred in ["", "--mark-pred"]:
         for directed in ["", "--undirected"]:
-            for dataset in ['soc-LiveJournal1',
-                            'kron_g500-logn21',
-                            'delaunay_n21',
-                            'delaunay_n24',
-                            'belgium_osm',
-                            'europe_osm',
-                            'road_usa',
-                            'cit-Patents',
-                            'ak2010',
-                            'delaunay_n13',
-                            'coAuthorsDBLP']:
-                os.system("../../gunrock-build/bin/%s market ../dataset/large/%s/%s.mtx --src=0 %s %s --idempotence --iteration-num=10 --quiet --jsondir=." % (binary, dataset, dataset, mark_pred, directed))
+            for dataset in ['gh001',
+                            'gh002',
+                            'gh003',
+                            'gh004',
+                            'gh005',
+                            'gh006',
+                            'gh007',
+                            'gh008',
+                            'gh009',
+                            'gh010',
+                            'gh011',
+                            'gh012',
+                            'gh013',
+                            'gh014',
+                            'gh015',
+                            'gh016']:
+                os.system("../../gunrock/build/bin/%s market ../../maps/%s.mtx --src=0 %s %s --idempotence --iteration-num=10 --quiet --jsondir=." % (binary, dataset, mark_pred, directed))
